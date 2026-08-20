e742e3a48b43271930732ea30e7600c660fa7ab1
vanshpandey899 <pandeyvansh899@gmail.com>
2026-08-21 02:01:52 +0530

    Add files via upload

---
+++ PlaySync_GameZone.js
@@ -0,0 +1,15 @@
+let nextButton = document.querySelector(".next_btn");
+let videos = document.querySelector(".hero_video");
+
+const vid_list = ["hero-1.mp4","hero-2.mp4","hero-3.mp4","hero-4.mp4"];
+
+let index = 0;
+
+nextButton.addEventListener("click",function(vid_list){
+    index += 1;
+    videos.src = vid_list[index];
+
+    if(index === 3){
+        index = -1;
+    }
+})
